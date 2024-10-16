ea83239149a8c5d39159732ca4b43af52e0e54ea
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-15 23:25:32 -0500

    initial commit

---
+++ src/main/java/com/riwi_project/Riwi_project/RiwiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.riwi_project.Riwi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi_project/Riwi_project/RiwiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi_project.Riwi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
