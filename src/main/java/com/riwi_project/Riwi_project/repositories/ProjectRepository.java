1ed9569502a18d564a792e40dc496e8377dd499a
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-16 17:29:26 -0500

    create TaskRepo

--- src/main/java/com/riwi_project/Riwi_project/repositories/ProjectRepository.java
+++ src/main/java/com/riwi_project/Riwi_project/repositories/ProjectRepository.java
@@ -3,5 +3,5 @@ package com.riwi_project.Riwi_project.repositories;
 import com.riwi_project.Riwi_project.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectInterface extends JpaRepository<ProjectEntity, Long> {
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
 }
