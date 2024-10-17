ab564a68685e85a6d167c371cbba31b7b2ba9911
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-16 19:55:43 -0500

    create UserLogin

--- src/main/java/com/riwi_project/Riwi_project/services/implementation/JpaUserDetailsService.java
+++ src/main/java/com/riwi_project/Riwi_project/services/implementation/JpaUserDetailsService.java
@@ -4,26 +4,46 @@ import com.riwi_project.Riwi_project.entities.UserEntity;
 import com.riwi_project.Riwi_project.repositories.UserRepository;
 import com.riwi_project.Riwi_project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class JpaUserDetailsService implements UserService {
+public class JpaUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
-    @Override
-    public UserEntity save(UserEntity userEntity) {
-        return null;
-    }
 
     @Override
-    public Optional<UserEntity> findById(String id) {
-        return Optional.empty();
-    }
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //captamos el username y lo buscamos con nuestro metodo
+        Optional<UserEntity> userOptional = userRepository.findByUsername(username);
 
-    @Override
-    public List<UserEntity> findAll() {
-        return List.of();
+        //VERIFICAMOS SI EXISTIO O NO
+        if(userOptional.isEmpty()){
+            throw new UsernameNotFoundException(String.format("Username %s no existe en el sistema", username));
+        }
+
+        UserEntity userEntity = userOptional.get();
+
+        //creamos una lista de roles, por medio del apiStream para transormarlo
+        List<GrantedAuthority> authorities = userEntity.getRoles().stream()
+                .map(roleEntity -> new SimpleGrantedAuthority(roleEntity.getName())).collect(Collectors.toList());
+
+        //retornamos una instancia de User, la cual es de SPRING SECURITY
+        return new User(userEntity.getUsername(),
+                userEntity.getPassword(),
+                userEntity.isEnabled(),
+                true,
+                true,
+                true,
+                authorities);
+        //PASAMOS TODOS LOS ATRIBUTOS, 3 TRUE Y LA LISTA CREADA ANTERIORMENTE
     }
 }
